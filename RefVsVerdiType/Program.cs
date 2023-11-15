5a872c770583d6c1995ee27a46cfd2083e3104ed
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-11-15 13:47:00 +0100

    Initial commit

---
+++ RefVsVerdiType/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RefVsVerdiType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
